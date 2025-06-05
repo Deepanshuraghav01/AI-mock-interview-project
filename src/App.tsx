ccf8e8b2d6cf3a16d22dd8c62e2bb6ba96bba102
deepanshu <deeputhakur72912gmail.com>
2025-06-05 23:30:05 +0530

    changes : Header Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,39 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import { PublicLayout } from "@/layouts/public-layout";
+import Authenticationlayout from "@/layouts/auth-layout";
+
+import HomePage from "@/routes/home";
+import { SignInPage } from "./routes/sign-in";
+import { SignUpPage } from "./routes/sign-up"; 
+import ProtectedRoutes from "./layouts/protected-routes";
+import { MainLayout } from "./layouts/main-layout";
 const App = () => {
-  return <div className="text-blue-500">App</div>
-};
+  return (
+    <Router>
+      <Routes>
+        {/* public routes */}
+        <Route element={<PublicLayout />}>
+        <Route index element={<HomePage />} />
+        </Route>
+        {/* authentication layout */}
+        <Route element={<Authenticationlayout />}>
+        <Route path="/signin/*" element={<SignInPage />} />
+        <Route path="/signup/*" element={<SignUpPage />} />
 
-export default App
+        </Route>
+        {/* protected routes */}
+        <Route element= {
+          <ProtectedRoutes>
+            <MainLayout/>
+          </ProtectedRoutes>}>
+        
+        
+        
+        </Route>  
+      </Routes>
+    </Router>
+
+  )
+};
+export default App;
